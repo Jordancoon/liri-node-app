12057c7040b29d19f32e364c55447df3bfa24719
jordan <jgcoon24@gmail.com>
2018-09-15 09:50:57 -0500

    keys.js liri.js npm

---
+++ liri.js
@@ -0,0 +1,235 @@
+//=============================================================================
+// Initialization
+//=============================================================================
+var keys = require("./keys");
+var request = require('request');
+var Spotify = require('node-spotify-api');
+var fs = require("fs");
+var firstRun = require('first-run');
+var chalk = require('chalk');
+var inquirer = require('inquirer');
+// GLOBAL VARIABLES
+// These used to be from process.argv, but now they're coming from prompts
+var command;
+var args;
+
+//=============================================================================
+// Functions
+//=============================================================================
+//-----------------------------------------------------------------------------
+// Prompts user for input
+function init() {
+  // If this is the first time the user is running the app, include title graphic
+  if(firstRun()) {
+    console.log('\033c'); // clears out the terminal... usually.
+    console.log(chalk.magenta("   __ _      _ "));
+    console.log(chalk.magenta("  / /(_)_ __(_)"));
+    console.log(chalk.yellow(" / / | | '__| |"));
+    console.log(chalk.green("/ /__| | |  | |"));
+    console.log(chalk.blue("\\____/_|_|  |_|"));
+    console.log(chalk.blue("Welcome to Liri, because siri just isn't good enough."));
+    console.log(chalk.gray("───────────────────────────────────────────"));
+  }
+  inquirer.prompt([
+    {
+      "name": 'commandChoice',
+      "message": chalk.magenta('What would you like to do?'),
+      "type": 'list',
+      "choices": ['Spotify a Song', 'Fetch Movie Info', 'Do What It Says', 'Clear Log', 'Exit Liri'],
+    },
+    {
+      type: 'input',
+      name: 'arg',
+      message: 'Cool, what song?',
+      when: function (answers) {
+        return answers.commandChoice==="Spotify a Song";
+      }
+    },
+    {
+      type: 'input',
+      name: 'arg',
+      message: 'Right On, which movie?',
+      when: function (answers) {
+        return answers.commandChoice==="Fetch Movie Info";
+      }
+    }
+  ])
+  .then(function(answers){
+    command = answers.commandChoice;
+    console.log(command);
+    args = answers.arg;
+    brain(answers.commandChoice, answers.arg);
+  });
+}
+//-----------------------------------------------------------------------------
+// The command center of the app
+function brain(command, args) {
+  switch (command) {
+    case "Spotify a Song":
+      spotifyIt(args);
+      break;
+    case "Fetch Movie Info":
+      movieThis(args);
+      break;
+    case "Do What It Says":
+      doIt();
+      break;
+    case "Clear Log":
+      clearFirstRun();
+      break;
+    case "Exit Liri":
+      endProgram();
+      break;
+    default:
+      console.log("Sorry, I don't know how to do that yet.");
+      init();
+  }
+}
+//-----------------------------------------------------------------------------
+// String utility, see: https://stackoverflow.com/a/196991/3424316
+function toTitleCase(str) {
+  return str.replace(/\w\S*/g, function(txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+}
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// Use node-spotify-api to fetch track info from Spotify
+function spotifyIt(song) {
+  if(song=="") {
+    song = "smells like teen spirit";
+  }
+  var output = "";
+  var spotify = new Spotify({
+    id: "58260e84ba394684867be6d00a5aa994",
+    secret: "990a58f70143438fb7d63e7281754b63"
+  });
+  spotify
+    .search({
+      type: 'track',
+      query: song
+    })
+    .then(function(response) {
+      var songInfo = response.tracks.items[0];
+      if (songInfo) {
+        output += "───────────────────────────────────────────\n";
+        output += chalk.green("SONG:   \"" + toTitleCase(song).trim() + "\"\n");
+        output += chalk.green("ARTIST: " + songInfo.artists[0].name+"\n");
+        output += chalk.green("ALBUM:  " + songInfo.album.name+"\n");
+        output += chalk.green("LINK:   " + songInfo.href+"\n");
+      } else {
+        output += chalk.green("Hmmm... I couldn't find that song. Try another one.\n");
+      }
+      output += "───────────────────────────────────────────\n";
+      logIt(output);
+      console.log(output);
+      init();
+    })
+    .catch(function(err) {
+      console.log(err);
+      init();
+    });
+}
+//-----------------------------------------------------------------------------
+// Use request to fetch movie info from OMDB
+function movieThis(title) {
+  var output = "";
+  if (title == "") {
+    title = "fury";
+  }
+  // Then run a request to the OMDB API with the movie specified
+  var queryUrl = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=a86db76";
+
+  // This line is just to help us debug against the actual URL.
+  request(queryUrl, function(error, response, body) {
+    // If the request was successful...
+    if (!error && response.statusCode === 200) {
+      var content = JSON.parse(body);
+      if (content.Title === undefined) {
+        output += "\n───────────────────────────────────────────\n";
+        output += chalk.yellow("Hmm... I couldn't find that movie. Try another one.\n");
+      } else {
+        // Then log the body from the site!
+        output += "───────────────────────────────────────────\n";
+        output += chalk.yellow("TITLE:           " + content.Title + "\n");
+        output += chalk.yellow("YEAR:            " + content.Year + "\n");
+        output += chalk.yellow("IMDB Rating:     " + content.Ratings[0].Value + "\n");
+        output += chalk.yellow("Rotten Tomatoes: " + content.Ratings[1].Value + "\n");
+        output += chalk.yellow("Country:         " + content.Country + "\n");
+        output += chalk.yellow("Language:        " + content.Language + "\n");
+        output += chalk.yellow("Plot:            " + content.Plot + "\n");
+        output += chalk.yellow("Actors:          " + content.Actors + "\n");
+      }
+    } else {
+      output += error;
+      init();
+    }
+    output += "\n───────────────────────────────────────────\n";
+    logIt(output);
+    console.log(output);
+    init();
+  });
+
+}
+//-----------------------------------------------------------------------------
+// Execute the contents of a text file
+function doIt() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    // If the code experiences any errors it will log the error to the console.
+    if (error) {
+      return console.log(error);
+    }
+    // Then split it by commas (to make it more readable)
+    var dataArr = data.split(",");
+    command = dataArr[0];
+    args = dataArr[1];
+    brain(command, args);
+  });
+}
+//-----------------------------------------------------------------------------
+// Log data to an external file
+function logIt(output) {
+  // Set up the output
+  var sep = "═══════════════════════════════════════════\n";
+  var argsText = "";
+  if(args) {
+    argsText = "Argument: "+args+"\n";
+  }
+  output = sep + "\n" + Date() + "\nCommand: "+command+"\n"+argsText+output;
+  // Write/append to the file
+  fs.appendFile("log.txt", output, function(err) {
+    // If the code experiences any errors it will log the error to the console.
+    if (err) {
+      return console.log(err);
+    }
+  });
+}
+//-----------------------------------------------------------------------------
+// Reset the app state
+function clearFirstRun() {
+  // Clear out log file
+  fs.writeFile("log.txt", "", function(err) {
+    // If the code experiences any errors it will log the error to the console.
+    if (err) {
+      return console.log(err);
+    }
+    // Otherwise, it will print: "log.txt was updated!"
+    console.log('\033c');
+    console.log("The log file was cleared! I feel... young. Fresh. New.");
+    console.log(chalk.gray("───────────────────────────────────────────"));
+    init();
+  });
+  // See https://www.npmjs.com/package/first-run
+  firstRun.clear();
+}
+//-----------------------------------------------------------------------------
+// Reset the app state
+function endProgram() {
+  return;
+}
+//=============================================================================
+// Runtime
+//=============================================================================
+//brain(command, args);
+init();
